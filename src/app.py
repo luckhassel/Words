5addc04faf24a2d223cc07ddc0e8e659ce6d1823
Guilherme <guilhermeluckfett@gmail.com>
2024-04-06 09:22:10 -0300

    feature: adding words sort use case

--- src/app.py
+++ src/app.py
@@ -3,3 +3,4 @@ from controllers.words import words_controller
    
 app = Flask(__name__)
 app.register_blueprint(words_controller)
+app.json.sort_keys = False
--- src/controllers/words.py
+++ src/controllers/words.py
@@ -7,7 +7,7 @@ container = Container()
 @words_controller.before_request
 def only_json():
     if not request.is_json and request.method == "POST": 
-        abort(400)
+        abort(415)
 
 @words_controller.route('/', methods=["GET"])
 def words_health_check():
@@ -19,9 +19,18 @@ def vowel_count():
         content = request.json
         use_case = container.words_vowel_count_use_case(content['words'])
         return jsonify(use_case.vowel_count())
+    except KeyError:
+        abort(400)
     except:
         abort(422)
-
+    
 @words_controller.route('/sort', methods=["POST"])
 def sort():
-    return 'sort'
+    try:
+        content = request.json
+        use_case = container.words_sort_use_case(content['words'], content['order'])
+        return jsonify(use_case.sort())
+    except KeyError:
+        abort(400)
+    except:
+        abort(422)
